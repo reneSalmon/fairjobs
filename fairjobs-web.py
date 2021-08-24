783d8f8f9a831f64663bd8a00611006e616d5c61
René Salmon <rene.salmon@outlook.de>
2021-08-24 14:41:36 +0200

    code ready to be deployed to production

---
+++ fairjobs-web.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+'''
+# Fairjobs - job search engine
+'''
+
+# Search field
+title = st.text_input('Enter a jobtitel or company...')
+
+
+# Search Button
+if st.button('search'):
+    # print is visible in server output, not in the page
+    print('button clicked!')
+    st.write('I was clicked 🎉')
