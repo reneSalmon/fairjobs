d9efff4c698d89d9611241de39e1ffdaf8758ac1
René Salmon <rene.salmon@outlook.de>
2021-08-24 15:15:52 +0200

    code ready to be deployed to production

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
- web: sh setup.sh && streamlit run jobfair-web.py
+ web: sh setup.sh && streamlit run fairjobs-web.py
--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ mkdir -p ~/.streamlit/
 
 echo "\
 [general]\n\
-email = \"your-email@domain.com\"\n\
+email = \"rene.salmon@outlook.de\"\n\
 " > ~/.streamlit/credentials.toml
 
 echo "\
