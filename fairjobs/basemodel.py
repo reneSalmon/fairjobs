dc4eb0d83ba782e7a48fffdbc7cc1ac2a2322bd1
Komiljon Nasrullaev <komiljon.nasrullayev@gmail.com>
2021-09-01 17:42:12 +0200

    finally done

--- fairjobs/basemodel.py
+++ fairjobs/basemodel.py
@@ -94,15 +94,19 @@ class BaseModel(object):
                         if masc_word in word.lower():
                             self.List_for_annotation.append((word + ' ', "neutral", "#fea"))
                             flag_neut = True
-                    if flag_neut == False:
-                        self.List_for_annotation.append((word + ' ', "female", "#faa"))
-                        flag_fem = True
-
-            for masc_word in self.masc_words_list:
-                if masc_word in word.lower():
-                    if flag_neut == False:
-                        self.List_for_annotation.append((word + ' ', "male", "#8ef"))
-                        flag_masc = True
+                            break
+                    else:
+                        if flag_neut == False:
+                            self.List_for_annotation.append((word + ' ', "female", "#faa"))
+                            flag_fem = True
+                            break
+            else:
+                for masc_word in self.masc_words_list:
+                    if masc_word in word.lower():
+                        if flag_neut == False:
+                            self.List_for_annotation.append((word + ' ', "male", "#8ef"))
+                            flag_masc = True
+                            break
 
             if flag_neut == False and flag_fem == False and flag_masc == False:
                 self.List_for_annotation.append(word + ' ')
