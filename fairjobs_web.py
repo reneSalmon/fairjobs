d57ab4c90ab29d77722a1214e34bcfadfbbb661c
René Salmon <rene.salmon@outlook.de>
2024-08-26 20:07:51 +0200

    thumbs best for now2

--- fairjobs_web.py
+++ fairjobs_web.py
@@ -332,22 +332,7 @@ def app():
                 col7.metric(label='personal development',
                             value=row['personal development'])
 
-                #st.write(f"city: {row['loc']}")
-
-                container = st.container()
-
-                col1, col2 = container.columns(2)
-
-                with col1:
-                    st.write(f"city: {row['loc']}")
-
-                with col2:
-                    # Thumbs
-                    selected = st.feedback("thumbs", key=f"feedback_{index}_{row['job_title']}")
-                    if selected is not None:
-                        st.markdown(f"You selected: {sentiment_mapping[selected]}")
-
-                st.write(annotated_text(*row['list_for_annotation']))
+                st.write(f"city: {row['loc']}")
 
                 ##Feedback widget with unique key
 
