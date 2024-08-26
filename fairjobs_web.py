dd212643c7e43635b67141bf0fa8141bf1dcc286
René Salmon <rene.salmon@outlook.de>
2024-08-26 17:05:20 +0200

    fixed ParserError: Error tokenizing data. C error: Expected 2143 fields in line 87,saw 2182

--- fairjobs_web.py
+++ fairjobs_web.py
@@ -56,7 +56,7 @@ from st_files_connection import FilesConnection
 # Create connection object and retrieve file contents.
 # Specify input format is a csv and to cache the result for 600 seconds.
 conn = st.connection('gcs', type=FilesConnection)
-df = conn.read("fairjobsdata/data_data_data_full_df_web_gd.csv", input_format="csv", ttl=600)
+df = conn.read("fairjobsdata/data_data_data_full_df_web_gd.csv", input_format="csv", ttl=600, sep=";")
 
 # Print results.
 for row in df.itertuples():
