e2621bb458e46398e5e9aa12c8de524bb09da5c9
René Salmon <rene.salmon@outlook.de>
2024-09-03 17:45:34 +0200

    tokenizer3

--- tokenizer_app.py
+++ tokenizer_app.py
@@ -2,39 +2,37 @@ import streamlit as st
 from transformers import BertTokenizer
 
 
-def app():
+# Load the model's tokenizer
+pretrained_weights = 'deepset/gbert-large'
+tokenizer = BertTokenizer.from_pretrained(pretrained_weights)
 
-    # Load the model's tokenizer
-    pretrained_weights = 'deepset/gbert-large'
-    tokenizer = BertTokenizer.from_pretrained(pretrained_weights)
+# Function definition for tokenizing and print output
+def tokenize_text(text):
+    # Tokenize the text
+    token_list = tokenizer.tokenize(text)
 
-    # Function definition for tokenizing and print output
-    def tokenize_text(text):
-        # Tokenize the text
-        token_list = tokenizer.tokenize(text)
+    # Display tokenized sentences
+    st.write("Tokenized Sentences:")
+    sentence_list = []
+    for token in token_list:
+        sentence_list.append(token)
+        if token == '.':
+            st.write(sentence_list)
+            sentence_list = []
 
-        # Display tokenized sentences
-        st.write("Tokenized Sentences:")
-        sentence_list = []
-        for token in token_list:
-            sentence_list.append(token)
-            if token == '.':
-                st.write(sentence_list)
-                sentence_list = []
+    # Display the number of tokens
+    st.write('Die Anzahl der Tokens ist:', len(token_list))
 
-        # Display the number of tokens
-        st.write('Die Anzahl der Tokens ist:', len(token_list))
+# Streamlit app layout
+st.title('Text Tokenizer using GBERT')
+st.write("Please enter text to tokenize:")
 
-    # Streamlit app layout
-    st.title('Text Tokenizer using GBERT')
-    st.write("Please enter text to tokenize:")
+# Text input for user to enter text (initially empty)
+input_text = st.text_area('Enter your text here', value='')
 
-    # Text input for user to enter text (initially empty)
-    input_text = st.text_area('Enter your text here', value='')
-
-    # Tokenize and display output when button is clicked
-    if st.button('Analysiere die Text'):
-        if input_text.strip():  # Only tokenize if there is some input
-            tokenize_text(input_text)
-        else:
-            st.write("Please enter some text to tokenize.")
+# Tokenize and display output when button is clicked
+if st.button('Analysiere die Text'):
+    if input_text.strip():  # Only tokenize if there is some input
+        tokenize_text(input_text)
+    else:
+        st.write("Please enter some text to tokenize.")
